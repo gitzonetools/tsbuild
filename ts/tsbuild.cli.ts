d5852ce85d687e8f8d22ed800961119032166483
Phil Kunz <github@philkunz.com>
2019-01-27 02:42:58 +0100

    fix(core): update

--- ts/tsbuild.cli.ts
+++ ts/tsbuild.cli.ts
@@ -3,6 +3,9 @@ import * as tsbuild from './tsbuild.exports';
 
 const tsbuildCli = new plugins.smartcli.Smartcli();
 
+/**
+ * the standard task compiles anything in ts/ directory to dist directory
+ */
 tsbuildCli.standardTask().subscribe(argvArg => {
   if (process.env.CLI_CALL_TSBUILD === 'true') {
     tsbuild.compileGlobStringObject(
@@ -15,5 +18,23 @@ tsbuildCli.standardTask().subscribe(argvArg => {
     );
   }
 });
+
+/**
+ * the custom command compiles any customDir to dist_customDir
+ */
+tsbuildCli.addCommand('custom').subscribe(argvArg => {
+  const listedDirectories = argvArg._;
+  listedDirectories.shift();
+  const compilationCommandObject: {[key: string]: string} = {};
+  for (const directory of listedDirectories) {
+    compilationCommandObject['./' + directory] = './dist_' + directory;
+  };
+  tsbuild.compileGlobStringObject(
+    compilationCommandObject,
+    {},
+    process.cwd(),
+    argvArg
+  );
+})
 
 tsbuildCli.startParse();
