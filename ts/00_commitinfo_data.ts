4d37e880a337deb665e93aa76be9e56d2936122b
Philipp Kunz <gitlab@philkunz.com>
2022-05-25 11:52:42 +0200

    fix(core): update

---
+++ ts/00_commitinfo_data.ts
@@ -0,0 +1,8 @@
+/**
+ * autocreated commitinfo by @pushrocks/commitinfo
+ */
+export const commitinfo = {
+  name: '@gitzone/tsbuild',
+  version: '2.1.62',
+  description: 'TypeScript nightly to easily make use of latest features'
+}
